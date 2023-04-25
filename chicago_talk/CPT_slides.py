d37dda7fb850652d48b5975ac2022032303c8fa9
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2023-04-24 18:04:52 -0700

    Moved to Quarto presentation

--- chicago_talk/CPT_slides.py
+++ chicago_talk/CPT_slides.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Computational Political Theory
-# 
-# ## What Is It and Why Should You Care?
-# 
-# ### University of Chicago, April 28, 2023
+# ## Meaning, Understanding, and Quantification in the History of Ideas
 # 
 # ### Jeff Jacobs, [`jjacobs3@cs.stanford.edu`](mailto:jjacobs3@cs.stanford.edu)
+# 
+# ### University of Chicago, April 28, 2023
 # 
 # * This slideshow: https://jjacobs.me/cpt-talk
 # * All materials: https://github.com/jpowerj/cpt-talk
 
-# ## What It Is and What It Isn't
+# ## Computational Political Theory
+# 
+# ### What It Is and What It Isn't
 # 
 # * It is...
 #     * Adapting to the needs of humans/humanists/the humanities
@@ -21,19 +21,16 @@
 #     * Replacing humans/humanists/the humanities
 #     * "Automating" insights and research work (just the boring parts)
 
-# ## Meaning, Understanding, and Quantification in the History of Ideas
-# 
-# coolcoolio
-
-# In[1]:
-
-
-1+3
-
-
 # ## Background
 
-# Test
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+nyt_df = pd.read_csv("../results/nyt_sections_frozen.csv")
+nyt_df.head()
+
 
 # ## Models of Meaning and Context
 
